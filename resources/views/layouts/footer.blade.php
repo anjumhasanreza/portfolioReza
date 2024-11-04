9d018e885c056cfca85b9ae8f15fe1ef1e6d71ca
A Hasan Reza <33603359+anjumhasanreza@users.noreply.github.com>
2024-11-04 19:50:36 +0600

    My Work

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+        <p>&copy; 2023 Your Name. All rights reserved.</p>
+        <p>Follow me on <a href="#">LinkedIn</a> | <a href="#">GitHub</a> | <a href="#">Twitter</a></p>
+    </footer>
